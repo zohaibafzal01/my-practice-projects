62bb3594731218d80cd0c76b5d0b103a8410752a
Zohaib-CodeUpScale <zohaib.afzal@codeupscale.com>
2025-07-14 14:27:32 +0500

    url removed

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 import { selectUserInfo } from "@/redux/selectors/userSelectors";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const user = useSelector(selectUserInfo);
+  const user = useSelector(selectUserInfo);  
 
   if (!user?.token) {
     return <Navigate to="/login" replace />;
